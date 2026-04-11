32de5b1bbda1e90f107b8003200e372f5f97a0b1
Yusufolosun <olosun.m1904582@st.futminna.edu.ng>
2026-04-11 10:13:03 +0100

    feat(mobile): add main tab navigator

---
+++ mobile/src/app/navigation/MainTabsNavigator.tsx
@@ -0,0 +1,46 @@
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import {
+  AccountScreen,
+  HabitsScreen,
+  OverviewScreen,
+  PreviewScreen,
+} from '@/app/screens';
+import { mainTabScreenOptions } from './options';
+import { MAIN_TAB_ROUTES, type MainTabParamList } from './types';
+
+const Tab = createBottomTabNavigator<MainTabParamList>();
+
+export function MainTabsNavigator() {
+  return (
+    <Tab.Navigator screenOptions={mainTabScreenOptions}>
+      <Tab.Screen
+        name={MAIN_TAB_ROUTES.Overview}
+        component={OverviewScreen}
+        options={{
+          title: 'Overview',
+        }}
+      />
+      <Tab.Screen
+        name={MAIN_TAB_ROUTES.Habits}
+        component={HabitsScreen}
+        options={{
+          title: 'Habits',
+        }}
+      />
+      <Tab.Screen
+        name={MAIN_TAB_ROUTES.Preview}
+        component={PreviewScreen}
+        options={{
+          title: 'Preview',
+        }}
+      />
+      <Tab.Screen
+        name={MAIN_TAB_ROUTES.Account}
+        component={AccountScreen}
+        options={{
+          title: 'Account',
+        }}
+      />
+    </Tab.Navigator>
+  );
+}
